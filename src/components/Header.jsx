dc21b989a2d7a2ab378ca25d06491835339353fa
Manuel-Lauriola <manuelauriol@gmail.com>
2025-08-26 18:46:37 +0200

    [UPD] - header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="container">
+      <div className="row align-items-center p-10">
+        <div className="col-50">
+          <div id="logo">
+            <img src="../public/img/dc-logo.png" alt="Logo" />
+          </div>
+        </div>
+        <div className="col-50">
+          <ul id='menu'>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
