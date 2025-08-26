3fafb981fc4f8362c13ae6dc45ab7e153984a933
Manuel-Lauriola <manuelauriol@gmail.com>
2025-08-26 17:46:35 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
+  )
+}
+
+export default App
